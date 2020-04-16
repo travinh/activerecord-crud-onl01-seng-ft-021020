3abdf3cb7e4ff33e671c04957357e0cc736c5fdc
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-04-16 08:02:46 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -75,7 +75,7 @@ end
 def can_update_using_update_method
   # Update movie title to "Wat, huh?"
   Movie.create(title: "Wat?")
-  Movie.find_by(title="Wat?").update()
+  Movie.find_by(title="Wat?").update(title ='Wat, huh?')
   
 end
 
