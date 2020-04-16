f5a57a5cc6a8c1d5031106c608d01ad77c04051e
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-04-16 08:12:17 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -56,7 +56,8 @@ def can_find_by_multiple_attributes
   # title == "Title"
   # release_date == 2000
   # director == "Me"
-  Movie.find_by(title="Title", release_date=2000, director="Me")
+  
+  Movie.find_by(title: "Title", release_date: 2000, director: "Me")
 end
 
 def can_find_using_where_clause_and_be_sorted
